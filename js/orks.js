58274d13e015445308ce59188cdf4b3a360be5ca
chrop01 <91630967+0xProtocol@users.noreply.github.com>
2023-11-07 22:43:18 +0100

    docs: add comments + resources

--- js/orks.js
+++ js/orks.js
@@ -1,7 +1,10 @@
 
 export const create_ork = function (positiom) {
 
-    const orcScale = "0.0075 "
+    const orcScale = "0.0075 " //scale for case and ork
+    // const orcScale = "0.8 " for mage -> issues when clicking (actually you have to hit arm)
+    //general issue if you point at one point of the six -> it hits automatically without even clicking
+
 
     var ork = document.createElement("a-entity");
 
