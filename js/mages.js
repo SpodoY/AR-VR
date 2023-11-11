6a2994e8efc657394b31df07c217386a8dba4203
chrop01 <91630967+0xProtocol@users.noreply.github.com>
2023-11-11 12:14:08 +0100

    fix: add mage with better hitbox

--- js/mages.js
+++ js/mages.js
@@ -1,7 +1,9 @@
 export const create_mage = function (positiom) {
 
-    const mageScale = "0.8"
-    //general issue if you point at one point of the six -> it hits automatically without even clicking
+    //const mageScale = "0.8" mage -- general issue if you point at one point of the six -> it hits automatically without even clicking
+    const mageScale = "0.0655 " //mage2 + mage3  -- mage3 looks better
+    //const mageScale = "0.7 "
+
 
 
     var mage = document.createElement("a-entity");
